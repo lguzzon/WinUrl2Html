d7d5a87dc7cbfe568f66d397c7f223aa8cdca601
Luca Guzzon <luca.guzzon@gmail.com>
2014-03-01 16:05:02 +0100

    Removed Unnecessary semicolon. Code checked (jsHint) and formatted

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var shortRecursive = 'r',
     optionsRecursive = {
         alias: 'recursive',
@@ -12,19 +11,17 @@ var shortRecursive = 'r',
         describe: 'Start path to search in',
         defaultValue: '.'
     },
-    cli = require('cli').enable('status', 'version'),
+    cli = require('cli')
+        .enable('status', 'version'),
     packageJson = require('./package.json');
-
 if (packageJson) {
     cli.setApp(packageJson.name, packageJson.version);
 }
-
 cli.parse({
     recursive: [shortRecursive, optionsRecursive.describe],
     startpath: [shortStartPath, optionsStartPath.describe, 'string', optionsStartPath.defaultValue]
 });
-
-cli.main(function (aArgs, aOptions) {
+cli.main(function(aArgs, aOptions) {
     var fs = require('fs'),
         path = require('path'),
         async = require('async'),
@@ -32,15 +29,17 @@ cli.main(function (aArgs, aOptions) {
         lDirectoryQueue;
 
     function lDirectoryWorker(aTask, aTaskCallBack) {
-        fs.stat(aTask.path, function (aStatError, aStats) {
+        fs.stat(aTask.path, function(aStatError, aStats) {
             if (aStatError) {
                 aTaskCallBack(aStatError);
             } else if (aStats.isDirectory()) {
                 //cli.info('Processing :' + aTask.path);
-                fs.readdir(aTask.path, function (aReadDirError, aReadDirFiles) {
+                fs.readdir(aTask.path, function(aReadDirError, aReadDirFiles) {
                     if (!aReadDirError) {
-                        aReadDirFiles.forEach(function (aReadDirFile) {
-                            lDirectoryQueue.push({path: path.join(aTask.path, aReadDirFile)});
+                        aReadDirFiles.forEach(function(aReadDirFile) {
+                            lDirectoryQueue.push({
+                                path: path.join(aTask.path, aReadDirFile)
+                            });
                         });
                     }
                     aTaskCallBack(aReadDirError);
@@ -49,7 +48,8 @@ cli.main(function (aArgs, aOptions) {
                 if (aStats.isFile() && aTask.path.match(/\.[uU][rR][lL]$/)) {
                     cli.info('Replacing:' + aTask.path);
                     var lNewFileName = aTask.path.match(/(.*)\.[uU][rR][lL]$/)[1] + '.html',
-                        lURL = fs.readFileSync(aTask.path, 'utf8').match(/=(.*)/)[1],
+                        lURL = fs.readFileSync(aTask.path, 'utf8')
+                            .match(/=(.*)/)[1],
                         lNewContent = '<HTML><HEAD><META HTTP-EQUIV="Refresh" CONTENT="0; URL=XXX"></HEAD><BODY></BODY>'.replace('XXX', lURL);
                     fs.writeFileSync(lNewFileName, lNewContent, 'utf8');
                     fs.unlinkSync(aTask.path);
@@ -58,9 +58,10 @@ cli.main(function (aArgs, aOptions) {
             }
         });
     }
-
     cli.info('Working in: ' + path.resolve(process.cwd()));
     cli.info('Searching in: ' + lStartPath);
     lDirectoryQueue = async.queue(lDirectoryWorker, 32);
-    lDirectoryQueue.push({path: lStartPath});
+    lDirectoryQueue.push({
+        path: lStartPath
+    });
 });
