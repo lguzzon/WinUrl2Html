89de88683c568c3417bbf6fc1031e02dc3f269f1
Luca Guzzon <luca.guzzon@gmail.com>
2017-03-31 16:01:50 +0200

    Dependencies update
    Dependencies update

---
+++ runInDropBox.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+IF [%DEBUG%] EQU [2] (
+	ECHO ON	
+)
+
+SETLOCAL
+SET "TO_NUL= > nul 2> nul"
+IF [%DEBUG%] NEQ [] (
+	SET TO_NUL=	
+)
+
+
+
+SET "LOCAL_ERROR=0"
+SET "CURRENT_DIR=%CD%"
+SET "SCRIPT_DIR=%~dp0"
+
+node "%SCRIPT_DIR%app.js" -r -s "%SCRIPT_DIR%..\..\..\.."
+
+:LOCAL_EOF
+ENDLOCAL & EXIT /B %LOCAL_ERROR%
